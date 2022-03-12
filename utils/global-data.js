bdeb055800bedcbe637846adceaffa8b51353338
RevisionUpcountryVeggie <101471588+RevisionUpcountryVeggie@users.noreply.github.com>
2022-03-12 19:05:45 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Bothen.net';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Welcome to Bothen.net';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
